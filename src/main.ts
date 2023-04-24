ac83430bc2ec93d338aae80d391f0f4155e71c93
Nawsen <Nawsen@users.noreply.github.com>
2023-04-24 11:21:38 +0200

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,19 @@ import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { OverlayPanel, OverlayPanelModule } from 'primeng/overlaypanel';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule, OverlayPanelModule],
+  imports: [CommonModule, OverlayPanelModule, ButtonModule],
   template: `
-    <button (click)="toggle($event)">toggle panel here</button>
+    <button pButton pRipple label="Submit" class="p-button-success" (click)="toggle($event)"></button>
     <p-overlayPanel #overlay [dismissable]="false" [showCloseIcon]="false" appendTo="body">
-    Hello there
+      <ng-template pTemplate="content">
+        <h4>Custom Content</h4>
+      </ng-template>
     </p-overlayPanel>
   `,
 })
@@ -19,12 +23,10 @@ export class App {
   @ViewChild('overlay') private readonly overlay: OverlayPanel;
 
   toggle(event: any) {
-    try {
-      this.overlay.toggle(event);
-    } catch (e) {
-      // do nothing, primeng throws an error when closing popups with the close button
-    }
+    this.overlay.toggle(event);
   }
 }
 
-bootstrapApplication(App);
+bootstrapApplication(App, {
+  providers: [provideAnimations()],
+});
